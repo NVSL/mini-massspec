da8cdaa9b43ea56b4ebc511b56efa4a0745b638a
Narangerelt <55556893+Narangerelt@users.noreply.github.com>
2020-12-04 21:55:40 -0800

    Format the code

--- main.cpp
+++ main.cpp
@@ -14,21 +14,19 @@ typedef unsigned int MZ;
 typedef std::pair<MZ, Intensity> Peak;
 typedef std::vector<Peak> Spectrum;
 typedef std::vector<Spectrum> RawData;
-
 typedef std::pair<SID, Intensity> BucketPeak;
-
 typedef std::vector<BucketPeak> Bucket;
-
 typedef std::vector<Bucket> Index;
 
-static const MZ MAX_MZ = 2000;
+static const MZ MAX_MZ = 2000;  //max mz for indexing
 static const int num_buckets = 20; //# of bins per mz used for index
+
+
 
 RawData * load_raw_data(char *file) {
     RawData * spectra = new RawData();
     unsigned int file_id;
     unsigned int num_spectra;           //total number of spectra inside the file
-    unsigned int offset;                //starting offset from the file
     std::vector< std :: pair<unsigned int, unsigned int> > position; // starting position and ending position for each spectrum
 
     std::ifstream in(file, std::ios::in | std::ios::binary);
@@ -46,7 +44,7 @@ RawData * load_raw_data(char *file) {
         }
 
         for (unsigned int spec_idx = 0; spec_idx < num_spectra; ++spec_idx) {
-            in.seekg(position[spec_idx].first * 4 + 16); //setting the position to read to the first spectrum
+            in.seekg(position[spec_idx].first * 4 + 16); //setting the offset to read the spectrum
             unsigned int size = position[spec_idx].second - position[spec_idx].first - 4;
             Spectrum spectrum;
             //Populating peak info per spectrum
@@ -183,7 +181,10 @@ int main(int argc, char * argv[]) {
 		exit(1);
 	}
 
+    unsigned int total_spectra;
+
 	RawData * raw_data = load_raw_data(argv[1]);
+    total_spectra = raw_data->size();
 	std::cerr << "raw_data=\n";
 	dump_raw_data(raw_data);
 
@@ -209,7 +210,7 @@ int main(int argc, char * argv[]) {
 	auto reconstruct_end = std::chrono::high_resolution_clock::now();	
 	json_reconstruction(*reconstructed_spectra);
 
-    std::cerr << "Found " << reconstructed_spectra->size() << " candidates \n";
+    std::cerr << "Found " << reconstructed_spectra->size() << " candidates among "<< total_spectra << " spectra \n" ;
 	std::cerr << "Building the index took " << (std::chrono::duration_cast<std::chrono::nanoseconds>(index_build_end - index_build_start).count()+0.0)/1e9 << " s\n";
 	std::cerr << "Reconstruction took     " << (std::chrono::duration_cast<std::chrono::nanoseconds>(reconstruct_end - reconstruct_start).count()+0.0)/1e9 << " s\n";
 }
